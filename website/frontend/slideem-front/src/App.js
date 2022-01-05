75f9165fa03e1fd4d22c0bdbebc3c713d35ab36b
Chingis <chingisoinar@gmail.com>
2022-01-05 13:43:48 +0900

    QRcode generation example

--- website/frontend/slideem-front/src/App.js
+++ website/frontend/slideem-front/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {useEffect, useState} from 'react';
+import QRCode from 'qrcode';
 function App() {
+  const [src, setSrc] = useState('');
+  // Generate QR
+  const session_id = Date.now();
+  console.log(session_id)
+  useEffect(() => {
+    QRCode.toDataURL(session_id.toString() ).then(setSrc)
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <img src={src} />
+
   );
 }
 
