8d8f6bf1a9225d7b77a11282bbdb32abf04e1309
yessen <y.yesen.y@gmail.com>
2022-01-16 17:39:19 +0900

    Update QRScannerViewModel.swift

--- ios/Slideem/Slideem/VC/QRScanner/QRScannerViewModel.swift
+++ ios/Slideem/Slideem/VC/QRScanner/QRScannerViewModel.swift
@@ -9,6 +9,17 @@ import UIKit
 
 import AVFoundation
 
+protocol QRScannerViewModelDelegate: AnyObject {
+    func moveToJoystick()
+}
+
 class QRScannerViewModel {
+    
+    weak var delegate: QRScannerViewModelDelegate?
+    
+    public func gotQRCode(code: String) {
+        // TODO: Add checking the validation of qr with server
+        self.delegate?.moveToJoystick()
+    }
     
 }
